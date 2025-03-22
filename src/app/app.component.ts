63d0179ef6d0bdbcd6ab42e30328f5d42cbb5fe5
Elsobky20 <ahmedelsobky630@gmail.com>
2025-03-22 14:18:05 +0200

    create Project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { EmployeeFormComponent } from './employees/components/employee-form/employee-form.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet ,CommonModule , EmployeeFormComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
