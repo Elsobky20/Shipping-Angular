a8d870be90db16bac780b594a0682d1421713ab7
MohabYounis <167039645+MohabYounis@users.noreply.github.com>
2025-04-08 12:40:03 +0200

    merchantPreperation

---
+++ src/app/Merchant/components/merchant-details/merchant-details.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-merchant-details',
+  imports: [],
+  templateUrl: './merchant-details.component.html',
+  styleUrl: './merchant-details.component.css'
+})
+export class MerchantDetailsComponent {
+
+}
