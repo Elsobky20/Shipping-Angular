b6d81b5eb883041c7a65e05022c70e2faa07e578
MohabYounis <167039645+MohabYounis@users.noreply.github.com>
2025-04-10 00:11:04 +0200

    finishGetCities

--- src/app/City/components/cities/cities.component.ts
+++ src/app/City/components/cities/cities.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CityService } from '../../services/city.service';
+import { ICity } from '../../Interfaces/ICity';
 
 @Component({
   selector: 'app-cities',
@@ -6,6 +8,19 @@ import { Component } from '@angular/core';
   templateUrl: './cities.component.html',
   styleUrl: './cities.component.css'
 })
-export class CitiesComponent {
+export class CitiesComponent implements OnInit {
+  constructor(private cityService:CityService){}
 
+  cities!:ICity[];
+
+  ngOnInit(): void {
+    this.cityService.getAllCities().subscribe({
+      next: (response) => {
+        this.cities = response.data.cities;
+      },
+      error: (error) => {
+        console.log(error);
+      }
+    })
+  }
 }
