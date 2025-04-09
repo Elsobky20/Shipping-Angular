b9f0ef346f005e70ea11ec1350bc6ef35c98e39f
MohabYounis <167039645+MohabYounis@users.noreply.github.com>
2025-04-10 01:00:40 +0200

    resolveError

--- src/app/City/services/city.service.ts
+++ src/app/City/services/city.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
-import { ICityResponseData } from '../Interfaces/ICity';
 import { APIResponse } from '../../aModels/api-response';
+import { ICityResponseData } from '../Interfaces/icity-get';
 
 @Injectable({
   providedIn: 'root'
@@ -12,6 +12,10 @@ export class CityService {
 
   baseURL:string = "http://localhost:5050/api/City/all";
   getAllCities():Observable<APIResponse<ICityResponseData>>{
+    return this.http.get<APIResponse<ICityResponseData>>(this.baseURL);
+  }
+
+  getCityById(id:number):Observable<APIResponse<ICityResponseData>>{
     return this.http.get<APIResponse<ICityResponseData>>(this.baseURL);
   }
 }
