813c616cc43392b047bfea96e2f2c31c3608f35f
MohabYounis <167039645+MohabYounis@users.noreply.github.com>
2025-04-10 18:22:10 +0200

    addGeneralService

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,13 +4,13 @@ import { RouterOutlet } from '@angular/router';
 import { EmployeeFormComponent } from './employees/components/employee-form/employee-form.component';
 import { RoleListComponent } from "./Roles/components/role-list/role-list.component";
 import { LoginComponent } from './Login/components/login/login.component';
-import { CitiesComponent } from './City/components/cities/cities.component';
 import { SideBarComponent } from './Layout/side-bar/side-bar.component';
+import { CitiesComponent } from './City/components/cities/cities.component';
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CommonModule, EmployeeFormComponent, RoleListComponent, CitiesComponent, SideBarComponent, LoginComponent],
+  imports: [RouterOutlet, CommonModule, EmployeeFormComponent, RoleListComponent, CitiesComponent,SideBarComponent, LoginComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
