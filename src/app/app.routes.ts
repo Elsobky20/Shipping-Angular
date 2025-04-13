53c8b1c61828397877143db5671853770d7bb98d
Shereen Ahmed <Shereenahmed387@gmail.com>
2025-04-13 21:03:47 +0200

    Pagination&Search&EditDelivery

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,16 @@ import { DashboardComponent } from './Dashboard/dashboard.component';
 import { CitiesComponent } from './City/components/cities/cities.component';
 import { NotFoundComponent } from './NotFound/not-found.component';
 import { CityFormComponent } from './City/components/city-form/city-form.component';
+import { AllDeliveryComponent } from './Delivery/components/all-delivery/all-delivery.component';
+import { AddDeliveryComponent } from './Delivery/components/add-delivery/add-delivery.component';
 
 export const routes: Routes = [
   {path: '', redirectTo: 'city', pathMatch: 'full'},
   {path: 'dashboard', component: DashboardComponent},
   {path: 'city', component: CitiesComponent},
   {path: 'city/:id', component: CityFormComponent},
+  { path: 'deliveries', component: AllDeliveryComponent },
+  { path: 'delivery/add', component: AddDeliveryComponent },
+  { path: 'delivery/edit/:id', component: AddDeliveryComponent },
   {path: '**', component: NotFoundComponent}
 ];
