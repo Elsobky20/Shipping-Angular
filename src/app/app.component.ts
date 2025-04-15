68ba6b47a059a00755f6edd19f529fd2c88fc97c
Shereen Ahmed <Shereenahmed387@gmail.com>
2025-04-15 04:30:20 +0200

    editODelivery

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,15 +9,21 @@ import { CitiesComponent } from './City/components/cities/cities.component';
 import { AllDeliveryComponent } from "./Delivery/components/all-delivery/all-delivery.component";
 import { AddDeliveryComponent } from "./Delivery/components/add-delivery/add-delivery.component";
 import { DashboardComponent } from "./Dashboard/dashboard.component";
+import { Router } from '@angular/router';
+
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CommonModule, EmployeeFormComponent, RoleListComponent, CitiesComponent, SideBarComponent, LoginComponent, DashboardComponent, AllDeliveryComponent, AddDeliveryComponent],
+  imports: [RouterOutlet, CommonModule, SideBarComponent ],
 
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'Shipping-Angular';
+  constructor(private router: Router) {}
+  shouldShowLayout(): boolean {
+    return !this.router.url.includes('/login');
+  }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -9,9 +9,13 @@ import { LoginComponent } from './Login/components/login/login.component';
 import { MerchantsComponent } from './Merchant/components/merchants/merchants.component';
 import { MerchantFormComponent } from './Merchant/components/merchant-form/merchant-form.component';
 import { MerchantDetailsComponent } from './Merchant/components/merchant-details/merchant-details.component';
+import { EmployeeListComponent } from './employees/components/employee-list/employee-list.component';
+import { EmployeeFormComponent } from './employees/components/employee-form/employee-form.component';
+import { RoleListComponent } from './Roles/components/role-list/role-list.component';
+import { AddRoleComponent } from './Roles/components/add-role/add-role.component';
 
 export const routes: Routes = [
-  {path: '', redirectTo: 'merchant', pathMatch: 'full'},
+  {path: '', redirectTo: 'login', pathMatch: 'full'},
   {path: 'dashboard', component: DashboardComponent},
   {path: 'city', component: CitiesComponent},
   {path: 'city/:id', component: CityFormComponent},
@@ -23,5 +27,9 @@ export const routes: Routes = [
   { path: 'delivery/edit/:id', component: AddDeliveryComponent },
   {path: 'delivery/details/:id', component: AddDeliveryComponent},
   {path: 'login', component: LoginComponent},
+  {path: 'employee', component: EmployeeListComponent},
+  {path: 'employee/add', component: EmployeeFormComponent},
+  {path:'role',component:RoleListComponent},
+  {path:'role/add',component:AddRoleComponent},
   {path: '**', component: NotFoundComponent}
 ];
