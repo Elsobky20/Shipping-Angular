4f21bd65b7d6bac15b9983a2af6d301ce9d5635c
MohabYounis <167039645+MohabYounis@users.noreply.github.com>
2025-04-28 17:28:27 +0300

    EditWeightPrice

--- src/app/WeightPricing/components/wiegh-price-form/wiegh-price-form.component.ts
+++ src/app/WeightPricing/components/wiegh-price-form/wiegh-price-form.component.ts
@@ -1,68 +1,73 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpReqService } from '../../../GeneralSrevices/http-req.service';
-import { WieghPriceService } from '../../services/wiegh-price.service';
-import { FormsModule } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import Swal from 'sweetalert2';
+import { RouterLink } from '@angular/router';
+import { WieghPriceService } from '../../services/wiegh-price.service';
 
 @Component({
   selector: 'app-wiegh-price-form',
-  imports: [FormsModule],
+  imports: [ReactiveFormsModule, RouterLink],
   templateUrl: './wiegh-price-form.component.html',
   styleUrl: './wiegh-price-form.component.css'
 })
 export class WieghPriceFormComponent implements OnInit {
- 
-  formData = {
-    defaultWeight: 0,
-    additionalKgPrice: 0
-  };
+  weightObject!: any;
 
-  constructor(private weightService: WieghPriceService) { }
+  constructor(
+    private httpReqService:HttpReqService,
+    private wieghPriceService:WieghPriceService
+  ) {}
+
+  weightForm = new FormGroup({
+    defaultWeight: new FormControl('0', [Validators.required]),
+    additionalKgPrice: new FormControl('0', [Validators.required]),
+  })
+
+  get getDefaultWeight() {
+    return this.weightForm.controls['defaultWeight'];
+  }
+  get getAdditionalKgPrice() {
+    return this.weightForm.controls['additionalKgPrice'];
+  }
 
   ngOnInit(): void {
+    // Step 1: Get Weight Price Object
+    this.httpReqService.getById('WeightPricing', 1).subscribe({
+      next: (response) => {
+        this.weightObject = response;
+        this.getDefaultWeight.setValue(response.defaultWeight);
+        this.getAdditionalKgPrice.setValue(response.additionalKgPrice);
+      },
+      error: (error) => {
+        console.log(error)
+      }
+    })
   }
 
-  
-  createWeightPricing(): void {
-    const newWeight = {
-      defaultWeight: this.formData.defaultWeight,
-      additionalKgPrice: this.formData.additionalKgPrice
-    };
-  
-    this.weightService.create(newWeight).subscribe({
-      next: (res) =>  Swal.fire({
-                title: 'Created successfully!',
-                text: 'wiegh Price Setting Created successfully!',
-                icon: 'success',
-                confirmButtonText: 'Ok'
-              }),
-      error: (err) =>   Swal.fire({
-              title: "Can't add Wiegh Price Setting",
-              text: 'This Setting is already Added!',
-              icon: 'error',
-              confirmButtonText: 'Ok'
-            })
-    });
-  }
-  updateWeightPricing(): void {
-    const updatedWeight = {
-      defaultWeight: this.formData.defaultWeight,
-      additionalKgPrice: this.formData.additionalKgPrice
-    };
-  
-    this.weightService.update(updatedWeight).subscribe({
-      next: (res) =>  Swal.fire({
-        title: 'Updated successfully!',
-        text: 'wiegh Price Setting Updated successfully!',
-        icon: 'success',
-        confirmButtonText: 'Ok'
-      }),
-error: (err) =>   Swal.fire({
-      title: "Can't Updated Wiegh Price Setting",
-      text: 'This Setting Does not Exist!',
-      icon: 'error',
-      confirmButtonText: 'Ok'
+  weightHandler(){
+    this.wieghPriceService.edit(this.weightForm.value).subscribe({
+      next: (response) => {
+        Swal.fire({
+          icon: 'success',
+          title: 'Weight Setting updated!',
+          text: `Weight Setting has been updated successfully✔.`,
+          confirmButtonColor: '#28a745'
+        });
+      },
+      error: (error) => {
+          Swal.fire({
+            icon: 'error',
+            title: 'Error',
+            text: 'Something went wrong while updating the weight setting.',
+            confirmButtonColor: '#dc3545'
+          });
+        }
     })
-    });
+  }
+
+  cancelHandeler():void {
+    this.getDefaultWeight.setValue(this.weightObject.defaultWeight);
+    this.getAdditionalKgPrice.setValue(this.weightObject.additionalKgPrice);
   }
 }
